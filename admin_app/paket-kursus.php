8f8d793e0af2f638c2c939a7ae11bf498dda839c
haxorsprogramming <haxorsuinsu@gmail.com>
2021-05-15 04:52:06 +0700

    'update'

--- admin_app/paket-kursus.php
+++ admin_app/paket-kursus.php
@@ -5,7 +5,7 @@
             <i class="fas fa-plus-circle"></i> Tambah Paket Kursus</a>
         </div>
         <div class="row" style="padding-left:20px;margin-right:10px;">
-            <table id="tblPaketKursus"' class='table table-hover table-bordered table-stripped'>
+            <table id="tblPaketKursus" class="table table-hover table-bordered table-stripped">
                 <thead>
                     <tr>
                         <th>Nama Paket</th>
@@ -22,26 +22,33 @@
     <div id="divTambahPaketKursus" style="display: none;">
         <div>
             <a href="#!" class="btn btn-primary btn-icon icon-left" @click='kembaliAtc'>
-                <i class='fas fa-reply'></i> Kembali
+                <i class="fas fa-reply"></i> Kembali
             </a>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6 col-12 mt-3">
             <div class="form-group">
-                <label>Nama Kursus</label>
+                <label>Nama Paket</label>
                 <input type="text" class="form-control" id="txtNamaKursus">
             </div>
             <div class="form-group">
-                <label>Kategori</label>
+                <label>Keterangan</label>
+                <textarea class="form-control" style="resize: none;" id="txtKeterangan"></textarea>
+            </div>
+            <div class="form-group">
+                <label>Jenjang</label>
                 <select class="form-control" id="txtKategori">
                     <option value="Akademik">Akademik</option>
                     <option value="Bahasa">Bahasa</option>
                     <option value="Seni">Seni</option>
                 </select>
             </div>
-            
+            <div class="form-group">
+                <label>Harga / Jam</label>
+                <input type="number" class="form-control" id="txtHarga">
+            </div>
             <div>
-                <a href="#!" class="btn btn-primary btn-icon icon-left" @click="simpanAtc()">
-                    <i class='fas fa-save'></i> Simpan
+                <a href="#!" class="btn btn-primary btn-icon icon-left" @click="simpanAtc">
+                    <i class="fas fa-save"></i> Simpan
                 </a>
             </div>
         </div>
@@ -51,5 +58,22 @@
 </div>
 
 <script>
+
 $("#tblPaketKursus").dataTable();
+
+var divListKursus = new Vue({
+    el : '#divListKursus',
+    data : {
+
+    },
+    methods : {
+        tambahKursusAtc : function()
+        {
+            $("#divListKursus").hide();
+            $("#divTambahPaketKursus").show();
+            document.querySelector("#txtNamaKursus").focus();
+        }
+    }
+});
+
 </script>
